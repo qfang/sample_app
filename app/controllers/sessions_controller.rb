220ea2a7df5b83110caf39359f727c801d3ff20c
qfang <irisjacky99@yahoo.com.au>
2013-03-07 22:01:31 +1100

    finish up the update user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
 		#puts user.authenticate(params[:session][:password])
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			redirect_back_or user
 		else
 			flash.now[:error] = "Invalid email/password combination"
 			render 'new'
